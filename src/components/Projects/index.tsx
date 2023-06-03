bba4e2c78093bc965f9fbb9c07574223f682feb9
Eric Oliveira <oiericoi@hotmail.co>
2023-06-03 12:03:40 -0300

    feat: add projects component

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,13 @@
+import ListProjects from '../ListProjects'
+import './style.css'
+
+const Projects = () => {
+  return (
+    <div className="projects">
+      <h1 id="title">Projetos</h1>
+      <ListProjects />
+    </div>
+  )
+}
+
+export default Projects
