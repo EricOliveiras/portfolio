c94f1d5e04c0c534eddc3d1ba4cc2832201cd839
Eric Oliveira <oiericoi@hotmail.com>
2023-06-06 15:17:59 -0300

    feat: add footer components

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p id="p-footer">Desenvolvido por Eric Oliveira - 2023</p>
+    </div>
+  )
+}
+
+export default Footer
